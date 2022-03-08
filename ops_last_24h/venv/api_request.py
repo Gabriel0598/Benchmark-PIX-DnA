a6cdcc2a6246aa56ee6c8c56fbdde130952d0c24
Lucas Rocha <Lucas.Rocha2@br.ey.com>
2022-03-08 11:50:19 -0300

    Api Request dicionario-csv

--- ops_last_24h/venv/api_request.py
+++ ops_last_24h/venv/api_request.py
@@ -6,6 +6,7 @@ import pandas as pd
 import json
 from pip._vendor import requests
 from datetime import date
+import ast 
 
 # URL da API do BACEN - Volume e valor total das transacoes realizadas nas ultimas 24h;
 # Consulta inicial em 03/08/2022 as 09h17
@@ -16,6 +17,42 @@ info = json.loads(request.content)
 # df = pd.json_normalize(info['Horario'])
 df = pd.read_json(url)
 df.to_csv("teste.csv")
+
+arquivo_Csv = pd.read_csv("teste.csv")
+print(arquivo_Csv.head()) #imprime top 5 linhas do arquivo teste.csv
+
+#Ver as colunas do arquivo importado
+#print(arquivo_Csv.info())
+
+
+#adicionando colunas
+arquivo_Csv['Horario'] = ""
+arquivo_Csv['QuantidadeMedia'] = ""
+arquivo_Csv['TotalMedio']=""
+
+#criando lista de dados
+listaHorarios = []
+listaQuantidadeMedia = []
+listaTotalMedio = []
+
+for cadaLinha in arquivo_Csv['value']:
+    dicionario = ast.literal_eval(cadaLinha) #converter string do value para dictionary
+    listaHorarios.append(dicionario["Horario"])
+    listaQuantidadeMedia.append(dicionario["QuantidadeMedia"])
+    listaTotalMedio.append(dicionario["TotalMedio"])
+    
+dado = {
+    'Horario': listaHorarios,
+    'QuantidadeMedia': listaQuantidadeMedia,
+    'TotalMedio': listaTotalMedio
+}
+
+df = pd.DataFrame(dado)
+df.to_csv('teste4.csv', mode='w', index=False, header=True)
+print("Arquivo criado com sucesso!")
+    
+#arquivo_Csv.to_csv("teste3.csv", index=False)
+    
 
 # tran_diarias = request.json()
 # print(request.content)
